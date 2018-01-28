7334e336f16161750b6f938d22976424c597ad5e
Rizwan Kadiwal <rizwan.kadiwal@yahoo.ca>
2018-01-28 23:30:36 +0000

    Faker Update

--- challenge_faker.rb
+++ challenge_faker.rb
@@ -1,5 +1,5 @@
 require_relative 'ar.rb'
 
-Faker::Name.name      #=> "Christophe Bartell"
-
-Faker::Internet.email
+#Unable to do faker as got the following error:
+#gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
+#-bash: https://github.com/stympy/faker.git,: No such file or directorygit 
